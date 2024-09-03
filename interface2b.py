0d34306944dc434fc8f793b9e59710dfc67068bf
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-09-03 15:35:55 -0300

    Update interface2b.py

--- interface2b.py
+++ interface2b.py
@@ -7,7 +7,6 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 key = cv2. waitKey(1)
-img_count = 0
 
 # Load Dlib's face detector and recognition model
 detector = dlib.get_frontal_face_detector()
@@ -60,8 +59,6 @@ def match_face(face_descriptor, labels, descriptors):
 class App:
     def __init__(self, master):
         self.master = master
-        self.master.title('Face Recognition Software')
-        self.master.geometry('1000x1000')
         self.frame_counter = 0
         self.recognized_faces = {}
         self.labels, self.descriptors = prepare_training_data("images")
@@ -70,18 +67,6 @@ class App:
 
         self.camera_label = Label(self.master)
         self.camera_label.pack()
-
-        self.name_label = Label(self.master, text="Enter Name:")
-        self.name_label.pack()
-
-        self.name_entry = Entry(self.master)
-        self.name_entry.pack()
-
-        self.capture_button = Button(self.master, text="Capture", command=self.on_capture_click)
-        self.capture_button.pack(side=LEFT)
-
-        self.retrain_button = Button(self.master, text="Retrain", command=self.on_retrain_click)
-        self.retrain_button.pack(side=LEFT)
 
         self.cap = cv2.VideoCapture(0)
         self.update_frame()
@@ -106,8 +91,7 @@ class App:
                     
                 self.face_labels = new_labels
                 
-
-                
+    
 
             else:
                 for face in faces:
@@ -120,37 +104,11 @@ class App:
                 label = self.face_labels.get((x, y), "Unknown")
                 cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-            frame = cv2.resize(frame, (1000, int(1000 * frame.shape[0] / frame.shape[1])))
-            image = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
-            self.camera_label.configure(image=image)
-            self.camera_label.image = image
-            self.master.after(30, self.update_frame) #
-            img_count =+ 1 # ver como incrementar, a cada frame em que uma determinada label aparece, mais um em img_count
+            self.master.after(30, self.update_frame) 
             
-            photo = f"./predict/{name, img_count}.jpg"
+            photo = f"./predict/{name, self.frame_counter}.jpg"
             cv2.imwrite(photo, frame)
-            # coloca para cada label que ele reconhece, uma foto de predict e armazena na pasta predict
             
-
-        
-            
-        
-    def on_capture_click(self):
-        name = self.name_entry.get()
-        ret, frame = self.cap.read()
-
-        if ret:
-            path = f"./images/{name}"
-            if not os.path.exists(path):
-                os.mkdir(path)
-            picnumber = len(os.listdir(path))
-            cv2.imwrite(f'{path}/{picnumber}.png', frame)
-            messagebox.showinfo("Info", "Image Captured Successfully!")
-
-    def on_retrain_click(self):
-        self.labels, self.descriptors = prepare_training_data("images")
-        messagebox.showinfo("Info", "Model Retrained with Updated Images!")
-
 
 def main():
     root = Tk()
